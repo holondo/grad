6e45089f40d406ff248ac784deccfbf22199d99a
Igor Martins <cardozo@usp.br>
2019-10-21 17:31:52 -0300

    Implementing required stack functions

---
+++ 4Semestre/SCC0202/t2/carro.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "carro.h"
+
+carro check_in() {
+    carro x;
+    scanf("%d %d %d", x.placa, x.hor_cheg, x.hor_said);
+    return(x)
+}
+int disp(carro car) {
+
+}
+int rejeita(carro car) {
+
+}
+void registrar_carro(){
+
+}
+void imprimir_carro(){
+
+}
+void sair(){
+
+}
---
+++ 4Semestre/SCC0202/t2/patio1.c
@@ -0,0 +1,12 @@
+#include "patio1.h"
+
+void p1_criar(p1 *p, elem x) {
+    p->topo = x;
+}
+void p1_inserir(elem x) {
+    elem *aux = x;
+}
+
+elem p1_remover() {
+
+    }
