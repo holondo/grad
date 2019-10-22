34c7702b4fb8b6a16cdaa597c10c6bff56a2d919
Igor Martins <cardozo@usp.br>
2019-10-22 19:54:17 -0300

    Implementing functions p1_criar, p1_estaCheio, p1_estaVazio, p1_inserir and p1_remover

--- 4Semestre/SCC0202/t2/patio1.c
+++ 4Semestre/SCC0202/t2/patio1.c
@@ -1,12 +1,33 @@
 #include "patio1.h"
 
-void p1_criar(p1 *p, elem x) {
-    p->topo = x;
-}
-void p1_inserir(elem x) {
-    elem *aux = x;
+void p1_criar(p1 *p) {
+    p->topo = NULL;
+    return;
 }
 
-elem p1_remover() {
-
+int p1_estaCheio(p1 *p) {
+    int cont = 0;
+    elem *aux = p->topo;
+    while (aux != NULL) {
+        cont++;
+        aux = aux->prox;
     }
+    return(cont == 5 ? 1 : 0);
+}
+
+int p1_estaVazio(p1 *p) {
+    return(p->topo == NULL ? 1 : 0);
+}
+
+int p1_inserir(p1 *p, elem* x) { //elem x deve ser uma variável dinâmica
+    if (p1_estaCheio(p))
+        return 0; //erro pilha vazia
+    elem *aux = p->topo;
+    p->topo = x;
+    x->prox = aux;
+}
+
+
+int p1_remover(p1 *p, elem *x) {
+    
+}
